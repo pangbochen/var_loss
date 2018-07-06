217de27f236a50505e323715aeba86161524bc29
lian <lian@gmail.com>
2018-07-06 22:19:24 +0800

    update

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
 import os
+
+cmd_prefix = 'python main.py'
+
+cmd_list = [
+    '--vis_env var_loss_mix',
+    '--vis_env var_loss_org',
+    '--vis_env var_loss_std',
+    '--vis_env var_loss_alpha --alpha 0.3',
+    '--vis_env var_loss_alpha --alpha 0.4',
+    '--vis_env var_loss_course --alpha 0.5',
+    '--vis_env var_loss_course --alpha 0.4',
+]
+
+for cmd in cmd_list:
+    os.system(cmd_prefix+' '+cmd)
